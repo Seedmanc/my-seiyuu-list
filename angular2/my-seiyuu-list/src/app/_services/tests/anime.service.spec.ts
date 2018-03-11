8be8d350dd28f78652379cc34e22be4b70165e92
Dmitry <seedmanc@yahoo.com>
2018-03-11 14:44:12 +0300

    broken tests

--- angular2/my-seiyuu-list/src/app/_services/tests/anime.service.spec.ts
+++ angular2/my-seiyuu-list/src/app/_services/tests/anime.service.spec.ts
@@ -8,11 +8,29 @@ import {MessagesService} from "../messages.service";
 import {SeiyuuService} from "../seiyuu.service";
 import {RoutingServiceMock} from "./routing.service.mock";
 import {RoutingService} from "../routing.service";
-import {model} from "../../_models/tests/seiyuu.model.spec";
+import {model, model2} from "../../_models/tests/seiyuu.model.spec";
 import {Seiyuu} from "../../_models/seiyuu.model";
+import {Anime} from "../../_models/anime.model";
 
 let x;
-describe('AnimeService', () => {
+let roles =  [{
+    "name": "Character 1",
+    "main": false,
+    "_id": 1
+  },{
+    "name": "Character 2",
+    "main": true,
+    "_id": 1
+  },{
+    "name": "Character 1",
+    "main": true,
+    "_id": 2
+  },{
+    "name": "Character 3",
+    "main": false,
+    "_id": 3
+  }]
+fdescribe('AnimeService', () => {
   x = undefined;
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -47,23 +65,7 @@ describe('AnimeService', () => {
       service.selected$.subscribe(data => y = data);
 
       let loaded = Object.assign({}, model);
-      loaded.roles.push({
-        "name": "Character 1",
-        "main": false,
-        "_id": 1
-      },{
-        "name": "Character 2",
-        "main": true,
-        "_id": 1
-      },{
-        "name": "Character 1",
-        "main": true,
-        "_id": 2
-      },{
-        "name": "Character 3",
-        "main": false,
-        "_id": 3
-      },);
+      loaded.roles.push(...roles);
       seiyuuSvc.loadedSeiyuu$.next([new Seiyuu(loaded)]);
 
       expect(y).toBe(model._id);
@@ -71,7 +73,7 @@ describe('AnimeService', () => {
       backend.expectOne({
         url: `${env.mongoUrl}/collections/anime?apiKey=${env.apiKey}&f={"title":1,"pic":1}&q={"_id":{"$in":[1,2,3,2829]}}`,
         method:'GET'
-      }, 'GET to count the # of records in the anime DB').flush([
+      }, 'GET to load details from anime DB').flush([
           {_id:2829, title: 'Ie Naki Ko Remi', pic:'/images/anime/12/26250v.jpg'},
           {_id:1, title: 'title 1', pic:'/pic1.jpg'},
           {_id:2, title: 'title 2', pic:'/pic2.jpg'},
@@ -88,5 +90,97 @@ describe('AnimeService', () => {
       expect(JSON.stringify(x[2])).toBe(
 '{"_id":3,"rolesBySeiyuu":{"578":[{"name":"Character 3","main":false,"_id":578}]},"main":false,"link":"//myanimelist.net/anime/3","thumb":"//myanimelist.net/pic3v.jpg","mainCharacter":"Character 3","characters":[{"name":"Character 3","main":false}],"title":"title 3"}')
      })
-  )
+  );
+
+  fit('should display shared anime for multiple seiyuu, also for mainOnly',
+    inject([AnimeService, HttpTestingController, SeiyuuService, MessagesService],
+      (service:AnimeService, backend:HttpTestingController, seiyuuSvc:SeiyuuService, msgSvc:MessagesService) => {
+      let y;
+      let spy = spyOn(msgSvc, 'status');
+       // Anime.detailsCache = {};
+
+      service.displayAnime$.subscribe(data => x = data);
+      service.selected$.subscribe(data => y = data);
+
+      let loaded = Object.assign({}, model);
+      loaded.roles.push(...roles);
+
+      let loaded2 = Object.assign({}, model2);
+      loaded2.roles.push({
+        "name": "Character 1",
+        "main": true,
+        "_id": 2
+      },{
+        "name": "Character 3",
+        "main": false,
+        "_id": 3
+      },{
+        "name": "Character 4",
+        "main": false,
+        "_id": 4
+      });
+      seiyuuSvc.loadedSeiyuu$.next([new Seiyuu(loaded),new Seiyuu(loaded2)]);
+
+      expect(y).toBe(model2._id);
+      expect(spy).toHaveBeenCalledWith('3 shared anime found');
+      backend.expectOne({
+        url: `${env.mongoUrl}/collections/anime?apiKey=${env.apiKey}&f={"title":1,"pic":1}&q={"_id":{"$in":[2,3,2829]}}`,
+        method:'GET'
+      }, 'GET to load details from anime DB').flush([
+          {_id:2829, title: 'Ie Naki Ko Remi', pic:'/images/anime/12/26250v.jpg'},
+          {_id:2, title: 'title 2', pic:'/pic2.jpg'},
+          {_id:3, title: 'title 3', pic:'/pic3.jpg'},
+        ]
+      );
+
+      expect(Anime.detailsCache[2].title).toBe('title 2');
+      expect(Anime.detailsCache[3].pic).toBe('/pic3.jpg');
+
+      expect(JSON.stringify(x[0])).toBe(
+'{"_id":2,"rolesBySeiyuu":{"0":[{"name":"Character 1","main":true,"_id":0}],"578":[{"name":"Character 1","main":true,"_id":578}]},"main":false,"link":"//myanimelist.net/anime/2","thumb":"//myanimelist.net/pic2v.jpg","characters":[],"title":"title 2"}'
+      );
+      expect(JSON.stringify(x[1])).toBe(
+'{"_id":3,"rolesBySeiyuu":{"0":[{"name":"Character 3","main":false,"_id":0}],"578":[{"name":"Character 3","main":false,"_id":578}]},"main":false,"link":"//myanimelist.net/anime/3","thumb":"//myanimelist.net/pic3v.jpg","characters":[],"title":"title 3"}'
+      );
+      expect(JSON.stringify(x[2])).toBe(
+'{"_id":2829,"rolesBySeiyuu":{"0":[{"name":"Pierre","main":false,"_id":0}],"578":[{"name":"Pierre","main":false,"_id":578}]},"main":false,"link":"//myanimelist.net/anime/2829","thumb":"//myanimelist.net/images/anime/12/26250v.jpg","characters":[],"title":"Ie Naki Ko Remi"}'
+      );
+
+      service.mainOnly$.next(true);
+      expect(spy).toHaveBeenCalledWith('1 shared anime found');
+      backend.expectNone({
+          url: `${env.mongoUrl}/collections/anime?apiKey=${env.apiKey}&f={"title":1,"pic":1}&q={"_id":{"$in":[2]}}`,
+          method:'GET'
+        }, 'should not request cached anime again');
+      expect(JSON.stringify(x[0])).toBe(
+        '{"_id":2,"rolesBySeiyuu":{"0":[{"name":"Character 1","main":true,"_id":0}],"578":[{"name":"Character 1","main":true,"_id":578}]},"main":false,"link":"//myanimelist.net/anime/2","thumb":"//myanimelist.net/pic2v.jpg","characters":[],"title":"title 2"}'
+      );
+  }));
+
+  it('should display no shared anime for multiple seiyuu if none found',
+      inject([AnimeService, HttpTestingController, SeiyuuService, MessagesService],
+        (service:AnimeService, backend:HttpTestingController, seiyuuSvc:SeiyuuService, msgSvc:MessagesService) => {
+        let y;
+        let spy = spyOn(msgSvc, 'status');
+
+        service.displayAnime$.subscribe(data => x = data);
+        service.selected$.subscribe(data => y = data);
+
+        let loaded = Object.assign({}, model);
+        loaded.roles.push(...roles);
+
+        let loaded2 = Object.assign({}, model2);
+        loaded2.roles = [];
+        seiyuuSvc.loadedSeiyuu$.next([new Seiyuu(loaded),new Seiyuu(loaded2)]);
+
+        expect(y).toBe(model2._id);
+        expect(spy).toHaveBeenCalledWith('no shared anime found');
+
+        expect(JSON.stringify(x)).toBe('[]');
+
+        service.selected$.next(578);
+        expect(Anime.activeSeiyuu).toBe(578);
+    })
+  );
+
 });
