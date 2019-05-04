72f7ac697f7c12c29af21e2d0c7dc29076e18373
Dmitry <seedmanc@yahoo.com>
2019-05-04 19:24:56 +0300

    Anime chart tests
    test private methods too, duh
    fix test calls leaking to real requests

--- angular2/src/app/_services/tests/anime.service.spec.ts
+++ angular2/src/app/_services/tests/anime.service.spec.ts
@@ -9,8 +9,10 @@ import {RoutingServiceMock} from "./routing.service.mock";
 import {RoutingService} from "../routing.service";
 import {basicModel, model, model2} from "../../_models/tests/seiyuu.model.spec";
 import {Seiyuu} from "../../_models/seiyuu.model";
-import {Anime} from "../../_models/anime.model";
+import {Anime, HashOfRoles} from "../../_models/anime.model";
 import {mockList} from "./seiyuu.service.spec";
+import {of} from "rxjs/observable/of";
+import {BusService} from "../bus.service";
 
 let x;
 let roles =  [{
@@ -30,6 +32,50 @@ let roles =  [{
     "main": false,
     "_id": 3
   }];
+let hashOfRoles:HashOfRoles[] = [
+  {
+    1: [{
+      name: 'character1',
+      main: true,
+      _id: 11
+    }],
+    2: [{
+        name: 'character2',
+        main: true,
+        _id: 11
+       }, {
+        name: 'character3',
+        main: false,
+        _id: 11
+    }],
+    3: [{
+      name: 'character4',
+      main: false,
+      _id: 11
+    }],
+  },
+  {
+    1: [{
+      name: 'character5',
+      main: false,
+      _id: 22
+    }],
+    2: [{
+        name: 'character2',
+        main: true,
+        _id: 22
+       }, {
+        name: 'character6',
+        main: false,
+        _id: 22
+    }],
+    4: [{
+      name: 'character7',
+      main: false,
+      _id: 22
+    }],
+  }
+];
 describe('AnimeService', () => {
   x = undefined;
   let service: AnimeService;
@@ -174,5 +220,123 @@ describe('AnimeService', () => {
         expect(Anime.activeSeiyuu).toBe(578);
     })
   );
+
+  it('sharedAnime([...])', () => {
+     Anime.activeSeiyuu = 22;
+     expect(JSON.stringify(service['sharedAnime'](hashOfRoles))).toBe('[{"_id":1,"rolesBySeiyuu":{"11":[{"name":"character1","main":true,"_id":11}],"22":[{"name":"character5","main":false,"_id":22}]},"main":false,"link":"//myanimelist.net/anime/1","thumb":"","firstCharacter":"character5","characters":[{"name":"character5","main":false}],"title":"1"},{"_id":2,"rolesBySeiyuu":{"11":[{"name":"character2","main":true,"_id":11},{"name":"character3","main":false,"_id":11}],"22":[{"name":"character2","main":true,"_id":22},{"name":"character6","main":false,"_id":22}]},"main":true,"link":"//myanimelist.net/anime/2","thumb":"","firstCharacter":"character2","characters":[{"name":"character2","main":true},{"name":"character6","main":false}],"title":"2"}]');
+
+     Anime.activeSeiyuu = 11;
+     expect(JSON.stringify(service['sharedAnime'](hashOfRoles))).toBe('[{"_id":1,"rolesBySeiyuu":{"11":[{"name":"character1","main":true,"_id":11}],"22":[{"name":"character5","main":false,"_id":22}]},"main":true,"link":"//myanimelist.net/anime/1","thumb":"","firstCharacter":"character1","characters":[{"name":"character1","main":true}],"title":"1"},{"_id":2,"rolesBySeiyuu":{"11":[{"name":"character2","main":true,"_id":11},{"name":"character3","main":false,"_id":11}],"22":[{"name":"character2","main":true,"_id":22},{"name":"character6","main":false,"_id":22}]},"main":true,"link":"//myanimelist.net/anime/2","thumb":"","firstCharacter":"character2","characters":[{"name":"character2","main":true},{"name":"character3","main":false}],"title":"2"}]');
+    }
+  );
+  it('sharedAnime([])', () => {
+     expect(JSON.stringify(service['sharedAnime']([]))).toBe('null');
+    }
+  );
+  it('sharedAnime([...],"main")', () => {
+     Anime.activeSeiyuu = 11;
+     expect(JSON.stringify(service['sharedAnime'](hashOfRoles, "main"))).toBe('[{"_id":2,"rolesBySeiyuu":{"11":[{"name":"character2","main":true,"_id":11}],"22":[{"name":"character2","main":true,"_id":22}]},"main":true,"link":"//myanimelist.net/anime/2","thumb":"","firstCharacter":"character2","characters":[{"name":"character2","main":true}],"title":"2"}]');
+    }
+  );
+  it('sharedAnime([...],"!main")', () => {
+     Anime.activeSeiyuu = 11;
+     expect(JSON.stringify(service['sharedAnime'](hashOfRoles, "!main"))).toBe('[{"_id":1,"rolesBySeiyuu":{"11":[{"name":"character1","main":true,"_id":11}],"22":[{"name":"character5","main":false,"_id":22}]},"main":true,"link":"//myanimelist.net/anime/1","thumb":"","firstCharacter":"character1","characters":[{"name":"character1","main":true}],"title":"1"}]');
+     Anime.activeSeiyuu = 22;
+     expect(JSON.stringify(service['sharedAnime'](hashOfRoles, "!main"))).toBe('[{"_id":1,"rolesBySeiyuu":{"11":[{"name":"character1","main":true,"_id":11}],"22":[{"name":"character5","main":false,"_id":22}]},"main":false,"link":"//myanimelist.net/anime/1","thumb":"","firstCharacter":"character5","characters":[{"name":"character5","main":false}],"title":"1"}]');
+    }
+  );
+  it('makeChart([...]) should display chart for 3 seiyuu', inject([SeiyuuService],
+    ( ) => {
+    let spy = spyOn(service, 'loadDetails').and.returnValue(of({}));
+    let x, hash = [...hashOfRoles];
+    hash.push({
+        '3': [{
+          name: 'character8',
+          main: true,
+          _id: 33
+        },{
+          name: 'character9',
+          main: false,
+          _id: 33
+        }],
+        '5': [{
+          name: 'character10',
+          main: false,
+          _id: 33
+        }]
+      });
+    service.displayChart$.subscribe(_ => x=_);
+    service['makeChart'](hash);
+
+    Anime.activeSeiyuu = 11;
+    expect(JSON.stringify(x)).toBe('[[[],[{"_id":2,"rolesBySeiyuu":{"11":[{"name":"character2","main":true,"_id":11}],"22":[{"name":"character2","main":true,"_id":22}]},"main":true,"link":"//myanimelist.net/anime/2","thumb":"","firstCharacter":"character2","characters":[{"name":"character2","main":true}],"title":"2"}],[]],[[{"_id":1,"rolesBySeiyuu":{"11":[{"name":"character1","main":true,"_id":11}],"22":[{"name":"character5","main":false,"_id":22}]},"main":true,"link":"//myanimelist.net/anime/1","thumb":"","firstCharacter":"character1","characters":[{"name":"character1","main":true}],"title":"1"}],[],[]],[[{"_id":3,"rolesBySeiyuu":{"11":[{"name":"character4","main":false,"_id":11}],"33":[{"name":"character8","main":true,"_id":33},{"name":"character9","main":false,"_id":33}]},"main":false,"link":"//myanimelist.net/anime/3","thumb":"","firstCharacter":"character4","characters":[{"name":"character4","main":false}],"title":"3"}],[],[]]]');
+    expect(spy).toHaveBeenCalledWith([ 1, 2, 3 ]);
+   })
+  );
+
+
+  it('loadDetails() should request data', inject([RestService],
+    (rest: RestService ) => {
+    let spy = spyOn(rest, 'mongoCall').and.returnValue(of([{_id:1}])),
+      x;
+    expect(Anime.detailsCache[1]).toBeFalsy();
+
+    service['loadDetails']([1,2,3]).subscribe(_ => x=_);
+
+    expect(spy).toHaveBeenCalledWith({
+      coll: 'anime',
+      mode: 'GET',
+      query: {
+        f: {title: 1, pic: 1},
+        q: {'_id': {'$in': [1,2,3]}}
+      }
+    });
+    expect(Anime.detailsCache[1]).toBeTruthy();
+
+    service['loadDetails']([ ]).subscribe(_ => x=_) ;
+    expect(x.length).toBeFalsy();
+   })
+  );
+
+
+  it('should call makeChart and load details if chart is enabled w/o redundant calls',
+    inject([SeiyuuService, RestService, BusService],
+      (seiyuuSvc:SeiyuuService, rest: RestService, bus: BusService) => {
+        let y;
+
+        let loaded = Object.assign({}, model);
+        loaded.roles.push(...roles);
+        let spy1 = spyOn<any>(service, 'makeChart').and.callThrough();
+        let spy2 = spyOn<any>(service, 'loadDetails').and.returnValue(of({}));
+
+        let loaded2 = Object.assign({}, model2);
+        loaded2.roles.push({
+          "name": "Character 1",
+          "main": true,
+          "_id": 2
+        },{
+          "name": "Character 3",
+          "main": false,
+          "_id": 3
+        },{
+          "name": "Character 4",
+          "main": false,
+          "_id": 4
+        });
+        bus.toggleChart = true;
+        seiyuuSvc.loadedSeiyuu$.next([new Seiyuu(loaded),new Seiyuu(loaded2)]);
+
+        expect(spy1).toHaveBeenCalled();
+        expect(spy2).toHaveBeenCalledTimes(1);
+
+        spy1.calls.reset();
+        spy2.calls.reset();
+
+        bus.toggleChart = false;
+        seiyuuSvc.loadedSeiyuu$.next([new Seiyuu(loaded),new Seiyuu(loaded2)]);
+
+        expect(spy1).toHaveBeenCalled();
+        expect(spy2).toHaveBeenCalledTimes(2);
+      }));
 
 });
